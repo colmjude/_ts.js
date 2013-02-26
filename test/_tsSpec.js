a275bb0e5b83b8bcf518c7fcedf658a4e6c5d6d4
Colm Britton <colmjude@gmail.com>
2013-02-26 17:55:10 +0000

    add tests for the isValidSpaceName func

--- test/_tsSpec.js
+++ test/_tsSpec.js
@@ -5,4 +5,18 @@ describe('_ts.js library', function() {
 		expect(window._ts).not.toBeNull();
 	});
 
+	describe('the isValidSpaceName function', function() {
+		var str = "colmjude";
+
+		it('should return true if the string is valid', function() {
+			expect(_ts.isValidSpaceName( str )).toBe(true);
+		});
+
+		it('should return false if the string contains special chars', function() {
+			str = str + "_";
+			expect(_ts.isValidSpaceName( str )).toBe(false);
+		});
+
+	});
+
 });
