6251c7553afb93e38639b97107b55552c381fe42
Colm Britton <colmjude@gmail.com>
2013-02-28 09:45:11 +0000

    Add tests for getSiteIconURL function

--- test/_tsSpec.js
+++ test/_tsSpec.js
@@ -19,4 +19,19 @@ describe('_ts.js library', function() {
 
 	});
 
+	describe('the getSiteIconURL function', function() {
+
+		it('should return url to default SiteIcon', function() {
+			var expected = "http://tiddlyspace.com/SiteIcon";
+			expect(_ts.getSiteIconURL()).toBe(expected);
+			expect(_ts.getSiteIconURL("GUEST")).toBe(expected);
+		});
+
+		it('should return url to the siteIcon of the given space', function() {
+			var spacename = "colmjude";
+			expect(_ts.getSiteIconURL( spacename )).toBe("http://tiddlyspace.com/bags/colmjude_public/tiddlers/SiteIcon");
+		});
+
+	});
+
 });
