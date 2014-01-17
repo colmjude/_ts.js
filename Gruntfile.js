f7dc949c7e40ca622d85c9aabdd661ee5c435f34
Colm Britton <colmjude@gmail.com>
2014-01-17 11:08:39 +0000

    Correct Gruntfile formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,46 +2,46 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-  grunt.initConfig({
-    lint: {
-      files: ['*.js', 'assets/*.js']
-    },
-    jasmine: {
-        src:['test/lib/jquery.js', '_ts.js'],
-		options: {
-			specs: 'test/*Spec.js',
-			helpers: 'test/lib/jasmine-jquery-1.3.1.js',
-			template: 'test/SpecRunner.tmpl'
-		}
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-		globals: {
-			"jQuery": true,
-			"tiddlyweb": true
-		}
-      },
-		files: {
-			src: ["Gruntfile.js", "_ts.js"]
-		}
-    }
-  });
+    grunt.initConfig({
+        lint: {
+            files: ['*.js', 'assets/*.js']
+        },
+        jasmine: {
+            src:['test/lib/jquery.js', '_ts.js'],
+            options: {
+                specs: 'test/*Spec.js',
+                helpers: 'test/lib/jasmine-jquery-1.3.1.js',
+                template: 'test/SpecRunner.tmpl'
+            }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    "jQuery": true,
+                    "tiddlyweb": true
+                }
+            },
+            files: {
+                src: ["Gruntfile.js", "_ts.js"]
+            }
+        }
+    });
 
-	// Default task.
-	grunt.registerTask('default', ["jshint", "jasmine"]);
-	// load tasks
-	grunt.loadNpmTasks('grunt-contrib-jasmine');
-	grunt.loadNpmTasks("grunt-contrib-jshint");
+    // Default task.
+    grunt.registerTask('default', ["jshint", "jasmine"]);
+    // load tasks
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks("grunt-contrib-jshint");
 
 };
